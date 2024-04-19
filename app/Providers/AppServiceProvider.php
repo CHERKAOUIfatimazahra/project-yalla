76a070709771566b31a0f3097380310e1287aeb4
CHERKAOUIfatimazahra <cherkaoui.fatimazahra97@gmail.com>
2024-04-19 16:09:29 +0100

    add in function register at appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,24 @@ use App\Repositories\AuthRepository;
 use App\Repositories\AuthRepositoryInterface;
 use App\Repositories\CategoryRepository;
 use App\Repositories\CategoryRepositoryInterface;
+use App\Repositories\EventRepository;
+use App\Repositories\EventRepositoryInterface;
+use App\Repositories\HomeRepository;
+use App\Repositories\HomeRepositoryInterface;
+use App\Repositories\OrganizerEventRepository;
+use App\Repositories\OrganizerEventRepositoryInterface;
+use App\Repositories\PaymentRepository;
+use App\Repositories\PaymentRepositoryInterface;
+use App\Repositories\PDFRepository;
+use App\Repositories\PDFRepositoryInterface;
+use App\Repositories\ReservationRepository;
+use App\Repositories\ReservationRepositoryInterface;
+use App\Repositories\SearchRepository;
+use App\Repositories\SearchRepositoryInterface;
+use App\Repositories\StaticRepository;
+use App\Repositories\StaticRepositoryInterface;
+use App\Repositories\TicketRepository;
+use App\Repositories\TicketRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Support\Facades\Gate;
@@ -23,6 +41,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(EventRepositoryInterface::class, EventRepository::class);
+        $this->app->bind(HomeRepositoryInterface::class, HomeRepository::class);
+        $this->app->bind(OrganizerEventRepositoryInterface::class, OrganizerEventRepository::class);
+        $this->app->bind(PDFRepositoryInterface::class, PDFRepository::class);
+        $this->app->bind(SearchRepositoryInterface::class, SearchRepository::class);
+        $this->app->bind(ReservationRepositoryInterface::class, ReservationRepository::class);
+        $this->app->bind(PaymentRepositoryInterface::class, PaymentRepository::class);
+        $this->app->bind(TicketRepositoryInterface::class, TicketRepository::class);
+        $this->app->bind(StaticRepositoryInterface::class, StaticRepository::class);
+
     }
 
     /**
